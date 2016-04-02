01170d20b092f8f782feac236928365276c7048c
Dmitry <lelekodmitry@gmail.com>
2016-04-03 02:08:14 +0300

    styles fix

--- index.js
+++ index.js
@@ -21,25 +21,25 @@ function Instanceof( obj, types ) {
 /* --------------------------------- _Instanceof --------------------------------- */
 
 function _Instanceof( obj, type ) {
-    
-    switch ( typeof type ) {
-        case 'number':
-            if ( isNaN( type ) ) type = 'nan';
-            else throw Error( 'type must be function, string, object or NaN' );
-        break;
-            
-        case 'string':
-            type = type.toLowerCase();
-        break;
-    }
+	
+	switch ( typeof type ) {
+		case 'number':
+			if ( isNaN( type ) ) type = 'nan';
+			else throw Error( 'type must be function, string, object or NaN' );
+		break;
+			
+		case 'string':
+			type = type.toLowerCase();
+		break;
+	}
 
 	switch ( type ) {
-            
+			
 		case 'nan':
 			return typeof obj == 'number' && isNaN( obj )
 		break;
-        
-        case Object:
+		
+		case Object:
 		case 'object':
 			return !Array.isArray( obj ) && obj !== null && typeof obj == 'object'
 		break;
@@ -73,13 +73,13 @@ function _Instanceof( obj, type ) {
 		case 'null':
 			return obj === null
 		break;
-        
-        case undefined:
+		
+		case undefined:
 		case 'undefined':
 			return obj === undefined
 		break;
-        
-        case Symbol:
+		
+		case Symbol:
 		case 'symbol':
 			return typeof obj == 'symbol'
 		break;
